ae050dcb3710b1dadf23399ce867e0de0204d9ed
Ibrahim Omar <62605376+Ibrahim2122@users.noreply.github.com>
2023-12-22 19:59:36 +0200

    Update forecast.js

--- Scripts/forecast.js
+++ Scripts/forecast.js
@@ -2,7 +2,7 @@ class Forecast {
     constructor() {
         this.key = 'ljZjn1WEkqJM6lyComZkvgv0Cg4lmHEL';
         this.weatherURL = 'https://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURL = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+        this.cityURL = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     }
     async updateCity(city) {
         const cityDetails = await this.getCity(city);
